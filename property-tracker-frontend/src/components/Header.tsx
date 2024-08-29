095283ff32e232dcb70344b0fe8985123431cc54
Granth Gharewal <gharewal12@outlook.com>
2024-08-29 17:11:32 +0530

    Added header and footer page content

--- property-tracker-frontend/src/components/Header.tsx
+++ property-tracker-frontend/src/components/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className='bg-blue-500 p-4 text-white'>
+      <nav className='container mx-auto flex justify-between'>
+        <Link
+          to='/'
+          className='font-bold'
+        >
+          Property Tracker
+        </Link>
+        <div>
+          <Link
+            to='/register'
+            className='mr-4'
+          >
+            Register Property
+          </Link>
+          <Link
+            to='/transfer'
+            className='mr-4'
+          >
+            Transfer Ownership
+          </Link>
+          <Link to='/history'>Property History</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
