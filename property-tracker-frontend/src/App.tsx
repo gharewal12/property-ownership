006ccb9929d57e81751f693ad3fab7bd000b2eac
Granth Gharewal <gharewal12@outlook.com>
2024-08-29 17:08:06 +0530

    Basic folder structure setup

--- property-tracker-frontend/src/App.tsx
+++ property-tracker-frontend/src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-  return <div className='App'>Property Ownership</div>;
-}
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import RegisterProperty from './pages/RegisterProperty';
+import TransferOwnership from './pages/TransferOwnership';
+import PropertyHistory from './pages/PropertyHistory';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className='flex flex-col min-h-screen'>
+        <Header />
+        <main className='flex-grow'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/register'
+              element={<RegisterProperty />}
+            />
+            <Route
+              path='/transfer'
+              element={<TransferOwnership />}
+            />
+            <Route
+              path='/history'
+              element={<PropertyHistory />}
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ property-tracker-frontend/src/pages/PropertyHistory.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PropertyHistory = () => {
+  return <div>PropertyHistory</div>;
+};
+
+export default PropertyHistory;
