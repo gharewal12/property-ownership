6e548f2ed83e7c564c08969a48f0f81571ca8976
Granth Gharewal <granthgharewal127@gmail.com>
2026-08-16 15:18:10 +0400

    Update footer copyright year to 2026

--- property-tracker-frontend/src/components/Footer.tsx
+++ property-tracker-frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className='bg-gradient-to-r lg:fixed lg:bottom-0 lg:left-0 w-full from-purple-600 via-pink-600 to-red-600 p-6 text-white text-center'>
       <p className='text font-semibold'>
-        &copy; 2024 PropertyTracker. All rights reserved.
+        &copy; 2026 PropertyTracker. All rights reserved.
       </p>
       <p className='text-sm'>
         Empowering secure and transparent property ownership with blockchain
